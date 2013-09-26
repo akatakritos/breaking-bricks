bef6e86860dff9178fa6491e62446ab09932fac0
Matt Burke <burkemd1+github@gmail.com>
2013-09-25 21:58:50 -0400

    remove puts

--- spec/lib/fetcher_spec.rb
+++ spec/lib/fetcher_spec.rb
@@ -5,7 +5,6 @@ describe Fetcher do
     it 'returns a nokogiri document' do
       fetcher = Fetcher.new
       page = fetcher.retiring_products_page
-      puts page.class
       page.should be_a(Nokogiri::HTML::Document)
     end
   end
