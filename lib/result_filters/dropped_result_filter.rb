a98aa223af0332a0d5b22d0f4e80fa93ad13142f
Matt Burke <burkemd1+github@gmail.com>
2013-09-30 13:26:46 -0400

    moved dropped result logic to shared base class

---
+++ lib/result_filters/dropped_result_filter.rb
@@ -0,0 +1,9 @@
+module ResultFilters
+  class DroppedResultFilter < BaseFilter
+    def filter(last, current)
+      last.scraper_results.each do |last_result|
+        yield last_result unless current.has_item?(last_result.item)
+      end
+    end
+  end
+end
--- lib/result_filters/retired_filter.rb
+++ lib/result_filters/retired_filter.rb
@@ -1,9 +1,4 @@
 module ResultFilters
-  class RetiredFilter < BaseFilter
-    def filter(last, current)
-      last.scraper_results.each do |last_result|
-        yield last_result unless current.has_item?(last_result.item)
-      end
-    end
+  class RetiredFilter < DroppedResultFilter
   end
 end
