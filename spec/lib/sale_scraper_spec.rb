0906b44c157ef2483128dff2c820f995e063bdca
Matt Burke <burkemd1+github@gmail.com>
2013-09-30 11:49:07 -0400

    specs for SaleScraper

--- spec/lib/sale_scraper_spec.rb
+++ spec/lib/sale_scraper_spec.rb
@@ -2,75 +2,63 @@ require 'spec_helper'
 
 describe SaleScraper do
   describe 'parse' do
-    before do
-    end
 
     let(:products) do
       scraper = SaleScraper.new(Nokogiri::HTML(open('spec/files/sales.html')), "example.com")
       scraper.get_products
     end
+
+    def get_product(code)
+      products.detect { |p| p[:code] == code }
+    end
       
 
     it 'should load something' do
-      @products.count.should == 33
-      @products.each do |product|
+      products.count.should == 33
+      products.each do |product|
         product[:name].should_not be_blank
         product[:code].should be_kind_of(Integer)
-        product[:link].should_not be_blank
+        product[:page].should_not be_blank
         product[:image].should_not be_blank
         product[:now_price].should be_kind_of(BigDecimal)
         product[:availability].should_not be_blank
         product[:availability_text].should_not be_blank
-        expect(product[:link]).to match /http:\/\/.*$/
+        expect(product[:page]).to match /http:\/\/.*$/
       end
     end
 
     it 'should have all the product codes' do
-      product_codes = [3937, 9679, 9476, 9678, 3183, 3848, 3856, 3939, 41017, 5695, 5794, 70113, 71000, 8065, 9469, 9489]
+      product_codes = [21201, 41020, 41021, 41022, 5001351, 70100,
+                       70101, 70102, 70104, 70105, 70106, 850443,
+                       850449, 850451, 850487, 850506, 850598,
+                       850611, 850612, 850614, 850674, 850775,
+                       850776, 850777, 853393, 853403, 853412, 9679,
+                       3920, 70103, 850514, 850615, 9678]
       product_codes.each do |code|
-        @products.detect { |prod| prod[:code] == code }.should_not be_nil
+        get_product(code).should_not be_nil
       end
     end
 
-    describe 'the standard block' do
-      it 'should have a now_price but no was_price' do
-        prod = @products.find { |prod| prod[:code] == 3937 }
-        prod[:code].should == 3937
-        prod[:now_price].should == 9.99
-        prod[:was_price].should be_nil
-        prod[:availability_text].should == 'Available Now'
+    it 'passes some spot checks' do
+      expect(get_product(21201)[:was_price]).to eq 29.99
+      expect(get_product(21201)[:now_price]).to eq 19.48
+
+      expect(get_product(70105)[:name]).to eq "Nest Dive"
+
+      expect(get_product(9678)[:availability]).to eq "call_to_check"
+    end
+
+
+    it 'has a now_price and was_price for each' do
+      products.each do |p|
+        expect(p[:was_price]).to be_a(BigDecimal)
+        expect(p[:now_price]).to be_a(BigDecimal)
       end
     end
 
-    describe 'a sale price block' do
-      it 'should have a now_price and a was_price' do
-        prod = @products.find { |p| p[:code] == 8065 }
-        prod[:now_price].should == 8.98
-        prod[:was_price].should == 10.99
-      end
-    end
-
-    describe 'an out of stock item' do
-      it 'should have an availability and an availability text' do
-        prod = @products.find { |p| p[:code] == 9476 }
-        prod[:availability].should == 'out_of_stock'
-        prod[:availability_text].should == "Temporarily out of stock"
-      end
-    end
-
-    describe 'call to check stock item' do
-      it 'should have an availability of :call_to_check and a text' do
-        prod = @products.find { |p| p[:code] == 9678 }
-        prod[:availability].should == 'call_to_check'
-        prod[:availability_text].should == 'Call to check product availability'
-      end
-    end
-
-    describe 'a sold out product' do
-      it 'should have availability :sold_out and a text' do
-        prod = @products.find { |p| p[:code] == 3183 }
-        prod[:availability].should == 'sold_out'
-        prod[:availability_text].should == "Sold Out"
+    it 'has a different now_prie and was_price for each' do
+      products.each do |p|
+        expect(p[:was_price]).to be > p[:now_price]
       end
     end
   end
